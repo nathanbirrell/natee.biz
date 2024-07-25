61bcc08f1ee5868d45cb63bf56bfb188c531024a
Nathan Birrell <nathanbirrell@gmail.com>
2024-07-25 18:42:37 +0200

    feat(resume): download pdf banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://natee.biz",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+
+    // TODO: add tailwind if needed
+    // tailwind(),
+  ],
 });
